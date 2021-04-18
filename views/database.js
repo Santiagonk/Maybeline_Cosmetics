770367a9ca732d3da6e3e3575b7cc53ce55078c4
Elsa Portilla <elsa.portilla@gmail.com>
2021-04-18 11:48:48 -0500

    updates

--- views/database.js
+++ views/database.js
@@ -1,14 +1,12 @@
-var obj = {
-    rick: 'morty'
-    }
-    console.log(obj)
-  
+    console.log('Inicializa')
+    
     var contenido = document.querySelector('#contenido')
     function traer() {
     fetch('http://localhost:8000/api/v1/products')
     .then(res => res.json())
     .then(data => {
-    console.log("In Front:", data)
+    console.log(data)
+    console.log(data)
     })
     }
     traer ()
