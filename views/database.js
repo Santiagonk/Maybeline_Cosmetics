0725298f0eebe80befa0b887ffa9d672c9f68028
Elsa Portilla <elsa.portilla@gmail.com>
2021-04-18 10:12:32 -0500

    API connect: success

--- views/database.js
+++ views/database.js
@@ -1,34 +1,3 @@
-var obj = {
-    nombre: 'Prueba 1',
-	muerte: 'ya pls',
-    last: 'Prueba mil',
-	rick: 'morty'
-}
-console.log(obj)
-
-// const express = require('express');
-// const router = express.Router();
-// const ProductService = require('../../services/services');
-
-// const productService = new ProductService();
-
-// router.get("/", async function(req, res, next) {
-//     const { category, product_type, tag_list, brand } = req.query;    
-//     console.log("Query: : ", req.query);    
-//     try {
-//       const products = await productService.getProducts({ category, product_type, tag_list, brand});
-  
-//       res.status(200).json({
-//         data: products,
-//         message: "products listed"
-//       });
-//     } catch (err) {
-//       next(err);
-//     }
-//   });
-
-// module.exports = router;
-
 var contenido = document.querySelector('#contenido')
 function traer() {
     console.log("Hola")
