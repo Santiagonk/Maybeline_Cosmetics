6cc08749ad203ebc8787b0e3469272026d4fde3a
Elsa Portilla <elsa.portilla@gmail.com>
2021-04-18 16:41:07 -0500

    Brach Script created

--- views/database.js
+++ views/database.js
@@ -1,16 +1,20 @@
     console.log('Inicializa')
     
-    var contenido = document.querySelector('#contenido')
+    var contenido = document.querySelector('.contenido')
     function traer() {
     fetch('http://localhost:8000/api/v1/products')
     .then(res => res.json())
     .then(data => {
         console.log(data)
         console.log(data.data)
-        console.log(data.data[1].name)
-        console.log(data.data[1].brand)
-        console.log(data.data[1].price)
-        console.log(data.data[1].image_link)
+        contenido.innerHTML = `
+            <p>Name: </p>
+            `
+
+        console.log(data.data[5].name)
+        console.log(data.data[5].brand)
+        console.log(data.data[5].price)
+        console.log(data.data[5].image_link)
         //console.log('Loop de nombres')
         //for (let i of data.data){
         //    console.log(i.name)
