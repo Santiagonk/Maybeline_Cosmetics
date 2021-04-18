b04027c3b22e3614b8d4aa45e9d0e5c78b20aa9a
Elsa Portilla <elsa.portilla@gmail.com>
2021-04-18 14:23:15 -0500

    Rode block with innerHTML

--- views/database.js
+++ views/database.js
@@ -5,11 +5,17 @@
     fetch('http://localhost:8000/api/v1/products')
     .then(res => res.json())
     .then(data => {
-    console.log(data)
-    console.log(data.data)
-    //console.log(data.data[100].name)
-    //console.log(data.data[100].price)
-    //console.log(data.data[100].image_link)
+        console.log(data)
+        console.log(data.data)
+        console.log(data.data[1].name)
+        console.log(data.data[1].brand)
+        console.log(data.data[1].price)
+        console.log(data.data[1].image_link)
+        //console.log('Loop de nombres')
+        //for (let i of data.data){
+        //    console.log(i.name)
+        //}
+    
     })
     }
     traer ()
