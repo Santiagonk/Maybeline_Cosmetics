260bff480a51f02bba939629709a3c635432df24
Elsa Portilla <elsa.portilla@gmail.com>
2021-04-18 10:14:18 -0500

    API connect

--- views/database.js
+++ views/database.js
@@ -1,12 +1,35 @@
-var contenido = document.querySelector('#contenido')
-function traer() {
+var obj = {
+    nombre: 'Prueba 1',
+    muerte: 'ya pls',
+    last: 'Prueba mil',
+    rick: 'morty'
+    }
+    console.log(obj)
+    // const express = require('express');
+    // const router = express.Router();
+    // const ProductService = require('../../services/services');
+    // const productService = new ProductService();
+    // router.get("/", async function(req, res, next) {
+    // const { category, product_type, tag_list, brand } = req.query;
+    // console.log("Query: : ", req.query);
+    // try {
+    // const products = await productService.getProducts({ category, product_type, tag_list, brand});
+    // res.status(200).json({
+    // data: products,
+    // message: "products listed"
+    // });
+    // } catch (err) {
+    // next(err);
+    // }
+    // });
+    // module.exports = router;
+    var contenido = document.querySelector('#contenido')
+    function traer() {
     console.log("Hola")
     fetch('http://localhost:8000/api/v1/products')
     .then(res => res.json())
-	.then(data => {
-		console.log("In Front:", data)
-	})
-    
-}
-
-traer ()
+    .then(data => {
+    console.log("In Front:", data)
+    })
+    }
+    traer ()
