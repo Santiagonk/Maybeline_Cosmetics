98292542951d04c74d12c1e7a3833ec1d394cda1
Santiago Salgado <jssalgadog@gmail.com>
2021-04-19 14:57:35 -0500

    Proof the filter by 10 elements

--- views/database.js
+++ views/database.js
@@ -7,15 +7,15 @@ fetch('http://localhost:8000/api/v1/products')
 .then(data => {
     console.log(data)
     console.log(data.data)
-    console.log(data.data[50].name)
-    console.log(data.data[50].brand)
-    console.log(data.data[50].price)
-    console.log(data.data[50].image_link)
+    console.log(data.data[5].name)
+    console.log(data.data[5].brand)
+    console.log(data.data[5].price)
+    console.log(data.data[5].image_link)
     contenido.innerHTML = `
-    <img class="img_product" src="https:${data.data[50].api_featured_image}"/>
-    <p>Name: ${data.data[50].name}</p>
-    <p>Brand: ${data.data[50].brand}</p>
-    <p>Price: $ ${data.data[50].price}</p>
+    <img class="img_product" src="https:${data.data[5].api_featured_image}"/>
+    <p>Name: ${data.data[5].name}</p>
+    <p>Brand: ${data.data[5].brand}</p>
+    <p>Price: $ ${data.data[5].price}</p>
     <div class="boton"><a href="url" alt="Buy button">BUY</a></div></span></div>
     `
     //console.log('Loop de nombres')
