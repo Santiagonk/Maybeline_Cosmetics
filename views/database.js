dda52f33ef1bb85e2f8007ea16b53d2c91975fae
Elsa Portilla <elsa.portilla@gmail.com>
2021-04-20 09:21:46 -0500

    Style fixes

--- views/database.js
+++ views/database.js
@@ -1,30 +1,24 @@
 console.log('Inicializa')
     
-var content = document.querySelector('.content')
+var display = document.querySelector('.display')
 function traer() {
-fetch('http://localhost:8000/api/v1/products?results=20')
+fetch('https://salty-beyond-87863.herokuapp.com/api/v1/products?results=20') 
 .then(res => res.json())
 .then(data => {
-    console.log(data)
-    console.log(data.data)
-    console.log(data.data[5].name)
-    console.log(data.data[5].brand)
-    console.log(data.data[5].price)
-    console.log(data.data[5].image_link)
-    contenido.innerHTML = `
-    <img class="img_product" src="https:${data.data[5].api_featured_image}"/>
-    <p>Name: ${data.data[5].name}</p>
-    <p>Brand: ${data.data[5].brand}</p>
-    <p>Price: $ ${data.data[5].price}</p>
-    <div class="boton"><a href="url" alt="Buy button">BUY</a></div></span></div>
-    `
-    //console.log('Loop de nombres')
-    //for (var i of data.data){
-    //    console.log(i.name)
-    //}
+    for(var i=0;i<data.data.length;i++){
+        container.innerHTML += `
+        <div class="element">
+        <img class="img_product" src="https:${data.data[i].api_featured_image}" alt="Cosmetico ${data.data[i].name}"/>
+        <p>Name: ${data.data[i].name}</p>
+        <p>Brand: ${data.data[i].brand}</p>
+        <p>Price: $ ${data.data[i].price}</p>
+        <div class="boton"><a href="url">view</a></div>
+        </div>`
+    }
 
 })
 }
 traer ()
 
-// https://salty-beyond-87863.herokuapp.com/api/v1/products
+// https://salty-beyond-87863.herokuapp.com/api/v1/products?results=20
+// http://localhost:8000/api/v1/products?results=20
