05f1f437fa0c3df51a2809b1dc6391cd861d4a5c
Elsa Portilla <elsa.portilla@gmail.com>
2021-04-20 16:19:18 -0500

    view button active

--- views/database.js
+++ views/database.js
@@ -2,11 +2,11 @@ console.log('Inicializa')
     
 var display = document.querySelector('.display')
 function traer() {
-fetch('https://salty-beyond-87863.herokuapp.com/api/v1/products?max=20&results=20') 
+fetch('https://salty-beyond-87863.herokuapp.com/api/v1/products?offset=20&results=20') 
 .then(res => res.json())
 .then(data => {
     console.log(data.data)
-
+    console.log(data.data[0].product_link)
     for(var i=0;i<data.data.length;i++){
         container.innerHTML += `
         <div class="element">
@@ -14,7 +14,7 @@ fetch('https://salty-beyond-87863.herokuapp.com/api/v1/products?max=20&results=2
         <p class="name">Name: ${data.data[i].name}</p>
         <p class="name">Brand: ${data.data[i].brand}</p>
         <p>Price: $ ${data.data[i].price}</p>
-        <div class="boton"><a href="url">view</a></div>
+        <div class="boton"><a href="${data.data[i].product_link}">view</a></div>
         </div>`
     }
 })
