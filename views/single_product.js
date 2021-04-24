08b3b98f2e95c93509ac3ad7355e2ffbff438618
Elsa Portilla <elsa.portilla@gmail.com>
2021-04-23 19:48:54 -0500

    Single product page created

---
+++ views/single_product.js
@@ -0,0 +1,26 @@
+console.log('Inicializa')
+
+var display = document.querySelector('.display')
+function traer() {
+fetch('https://salty-beyond-87863.herokuapp.com/api/v1/products?offset=0&results=1') 
+.then(res => res.json())
+.then(data => {
+    console.log(data.data)
+    console.log(data.data[0].product_link)
+    for(var i=0;i<data.data.length;i++){
+        container.innerHTML += `
+        <div class="element">
+        <img class="img_product" src="https:${data.data[i].api_featured_image}" alt="Cosmetico ${data.data[i].name}"/>
+        <p class="name">Name: ${data.data[i].name}</p>
+        <p class="name">Brand: ${data.data[i].brand}</p>
+        <p>Price: $ ${data.data[i].price}</p>
+        <div class="boton"><a href="${data.data[i].product_link}">view</a></div>
+        </div>`
+    }
+})
+}
+traer ()
+
+var today = new Date();
+var time = today.getHours();
+    console.log(today);
